34e0abcdceb5d688b4f0cc3ced371c3b097ace02
Nahid <75819596+nahidtislam@users.noreply.github.com>
2023-05-29 13:43:00 +0100

    Initial Commit

---
+++ ShapedProgressBar/ShapedProgressBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShapedProgressBarApp.swift
+//  ShapedProgressBar
+//
+//  Created by Nahid Islam on 29/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct ShapedProgressBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
