e7ba61b7e0d9ccca32603e511446a17e68982c9f
Nahid <75819596+nahidtislam@users.noreply.github.com>
2023-05-31 13:04:23 +0100

    the ui can now customise the colour of the progress bar

--- ShapedProgressBar/PlayView.swift
+++ ShapedProgressBar/PlayView.swift
@@ -9,47 +9,60 @@ import SwiftUI
 
 struct PlayView: View {
     
-    @State private var progess = 0.7
-    @State private var useSlider = true
+    @State private var progress = 0.0
+    @State private var leadColor = Color.mint
+    @State private var trailColor = Color.blue
     
-    let shapes: [(any Shape, UUID)] = [(Circle(), UUID()), (Rectangle(), UUID()), (RoundedRectangle(cornerRadius: 30), UUID())]
+    @State private var useSlider = true
     
     var body: some View {
         VStack {
             Label("hello world!", systemImage: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            TabView {
-                ShapedProgressBar(value: progess, shape: Circle(), thickness: 48)
-                    .padding()
-                ShapedProgressBar(value: progess, shape: Rectangle(), thickness: 48)
-                    .padding()
-                ShapedProgressBar(value: progess, shape: RoundedRectangle(cornerRadius: 30), thickness: 48)
-                    .padding()
-            }
-            .tabViewStyle(.page(indexDisplayMode: .never))
-                .animation(.default, value: progess)
+            shapesToFlipBy
+                .animation(.default, value: progress)
                 .frame(width: 280, height: 280)
             if useSlider {
                 slider
             } else {
                 incrementingButtons
             }
+            colorControl
         }
         .padding()
+        .onAppear {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                progress = 0.9
+            }
+        }
+    }
+    
+    private var shapesToFlipBy: some View {
+        TabView {
+            progress(shape: Circle()).padding()
+            progress(shape: Rectangle()).padding()
+            progress(shape: RoundedRectangle(cornerRadius: 30)).padding()
+            
+        }
+        .tabViewStyle(.page(indexDisplayMode: .never))
+    }
+    
+    func progress<S: Shape>(shape: S) -> ShapedProgressBar<S> {
+        ShapedProgressBar(shape, value: progress, thickness: 48, leadingColor: leadColor, trailingColor: trailColor)
     }
     
     private var incrementingButtons: some View {
         HStack {
             Button {
-                guard progess > 0 else { return }
-                progess -= 0.05
+                guard progress > 0 else { return }
+                progress -= 0.05
             } label: {
                 Label("reduce", image: "minus.square")
             }
-            Text(String(format: "%.2f", progess))
+            Text(String(format: "%.2f", progress))
             Button {
-                progess += 0.05
+                progress += 0.05
             } label: {
                 Label("increase", image: "plus.square.fill")
             }
@@ -59,8 +72,21 @@ struct PlayView: View {
     private var slider: some View {
         HStack {
             Text("progress")
-            Slider(value: $progess, in: 0.0...5.5, step: 0.01)
-            Text(String(format: "%.2f", progess))
+            Slider(value: $progress, in: 0.0...5.5, step: 0.01)
+            Text(String(format: "%.2f", progress))
+        }
+    }
+    
+    private var colorControl: some View {
+        HStack {
+            ColorPicker("lead", selection: $leadColor, supportsOpacity: false)
+                .padding()
+                .background(Material.thin)
+                .cornerRadius(30)
+            ColorPicker("trail", selection: $trailColor, supportsOpacity: false)
+                .padding()
+                .background(Material.thin)
+                .cornerRadius(30)
         }
     }
 }
